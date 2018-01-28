171a34109364f4fd4e5df4e42e8ad65fd75a68de
Jacob Knarr <jknarr@uoguelph.ca>
2018-01-28 05:35:34 -0500

    need to finish json post and response handlers

--- HomeChef/app/src/main/java/com/example/ama/homechef/MainActivity.java
+++ HomeChef/app/src/main/java/com/example/ama/homechef/MainActivity.java
@@ -10,6 +10,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 
 import com.ibm.watson.developer_cloud.android.library.camera.CameraHelper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
@@ -18,16 +26,25 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ImageClassific
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifier;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    RequestQueue MyRequestQueue;
+    JSONArray ingredients = new JSONArray();
     private VisualRecognition vrClient;
     private CameraHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -39,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize camera helper
         helper = new CameraHelper(this);
+
     }
 
     public void takePicture(View view) {
@@ -85,13 +103,38 @@ public class MainActivity extends AppCompatActivity {
                         classification.getClassifiers().get(0);
 
                 final StringBuffer output = new StringBuffer();
+
+
                 for(VisualClassifier.VisualClass object: classifier.getClasses()) {
                     if(object.getScore() > 0.7f)
-                        output.append("<")
+                       /* output.append("<")
                                 .append(object.getName())
-                                .append("> ")
-                                //.append(classifier);
+                                .append("> ");*/
+                        ingredients.put(object.getName());
                 }
+                output.append(ingredients.toString());
+
+                String url = "http://yourdomain.com/path";
+                StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //This code is executed if the server responds, whether or not the response contains data.
+                        //The String 'response' contains the server's response.
+                    }
+                }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //This code is executed if there is an error.
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> MyData = new HashMap<String, String>();
+                        MyData.put("Field", "Value"); //Add the data you'd like to send to the server.
+                        return MyData;
+                    }
+                };
+
+                MyRequestQueue.add(MyStringRequest);
 
                 runOnUiThread(new Runnable() {
                     @Override
