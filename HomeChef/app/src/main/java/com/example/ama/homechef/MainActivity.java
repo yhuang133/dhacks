5945be80b9d36a3d655db1742dc86534aa93c429
Jacob Knarr <jknarr@uoguelph.ca>
2018-01-28 03:51:18 -0500

    added watson api

--- HomeChef/app/src/main/java/com/example/ama/homechef/MainActivity.java
+++ HomeChef/app/src/main/java/com/example/ama/homechef/MainActivity.java
@@ -1,35 +1,109 @@
 package com.example.ama.homechef;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.ibm.watson.developer_cloud.android.library.camera.CameraHelper;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ImageClassification;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifier;
+
+import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private VisualRecognition vrClient;
+    private CameraHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Visual Recognition client
+        vrClient = new VisualRecognition(
+                VisualRecognition.VERSION_DATE_2016_05_20,
+                getString(R.string.api_key)
+        );
+
+        // Initialize camera helper
+        helper = new CameraHelper(this);
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    public void openCamera(View v){
-        Intent takePictureIntent = new Intent("android.media.action.IMAGE_CAPTURE");
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+    public void takePicture(View view) {
+        // More code here
+        helper.dispatchTakePictureIntent();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Intent intent = new Intent(this, HomeChef2.class);
-            startActivity(intent);
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        final Bitmap photo = helper.getBitmap(resultCode);
+        final File photoFile = helper.getFile(resultCode);
+
+        if(requestCode == CameraHelper.REQUEST_IMAGE_CAPTURE) {
+            // More code here
+            ImageView preview = findViewById(R.id.preview);
+            Matrix matrix = new Matrix();
+
+            matrix.postRotate(90);
+
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(photo,photo.getWidth(),photo.getHeight(),true);
+
+            Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap , 0, 0, scaledBitmap .getWidth(), scaledBitmap .getHeight(), matrix, true);
+            preview.setImageBitmap(rotatedBitmap);
         }
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                VisualClassification response =
+                        vrClient.classify(
+                                new ClassifyImagesOptions.Builder()
+                                        .images(photoFile)
+                                        .build()
+                        ).execute();
+
+                // More code here
+                ImageClassification classification =
+                        response.getImages().get(0);
+
+                VisualClassifier classifier =
+                        classification.getClassifiers().get(0);
+
+                final StringBuffer output = new StringBuffer();
+                for(VisualClassifier.VisualClass object: classifier.getClasses()) {
+                    if(object.getScore() > 0.7f)
+                        output.append("<")
+                                .append(object.getName())
+                                .append("> ")
+                                //.append(classifier);
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView detectedObjects =
+                                findViewById(R.id.detected_objects);
+                        detectedObjects.setText(output);
+                    }
+                });
+            }
+        });
     }
+
+
 }
